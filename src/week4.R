68781935c7ae8c8e0c515a5baef823d525c9c8e7
basacul <antelo.lucas@outlook.com>
2018-08-19 20:26:42 +0200

    Finished predictWord() for submission.

---
+++ src/week4.R
@@ -0,0 +1,13 @@
+# Tasks to accomplish
+#
+# 1. Explore new models and data to improve your predictive model.
+# 2. Evaluate your new predictions on both accuracy and efficiency. 
+#
+# Questions to consider
+#
+#What are some alternative data sets you could consider using?
+# 1. What are ways in which the n-gram model may be inefficient?
+# 2. What are the most commonly missed n-grams? Can you think of a reason why they would be missed and fix that?
+# 3. What are some other things that other people have tried to improve their model?
+# 4. Can you estimate how uncertain you are about the words you are predicting? 
+
